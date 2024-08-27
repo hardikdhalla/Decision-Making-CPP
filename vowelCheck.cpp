ef9b949663748a97de273f0bea0a9f372e78c7f3
hardikdhalla <dhallahardik31@gmail.com>
2024-08-27 13:09:58 +0530

    Add files via upload

---
+++ vowelCheck.cpp
@@ -0,0 +1,24 @@
+//Name: Hardik Dhalla
+//PRN: 23070123058
+//AIM - To check whether an alphabet is vowel or consonant
+#include <iostream>
+using namespace std;
+
+int main() { 
+  char a;
+  cout << "Enter a character: ";
+  cin >> a;
+  if (a=='a'||a=='e'||a=='i'||a=='o'||a=='u'||a=='A'||a=='E'||a=='I'||a=='O'||a=='U'){
+    cout << a << " is a vowel";
+  }
+  else{
+    cout << a << " is a consonant";
+  }
+return 0;
+    
+}
+
+/*Output-
+Enter a character: e
+e is a vowel
+*/
